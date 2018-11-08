31db858f7986889de471a2330be0d9dd49db07ef
itsAmarnath <40494037+itsAmarnath@users.noreply.github.com>
2018-11-08 20:27:42 +0000

    Add files via upload

---
+++ Attributeedit.ps1
@@ -0,0 +1,11 @@
+﻿import-module ActiveDirectory
+$users = import-csv -path C:\Users\ajp13577\Desktop\Book1.csv
+foreach ($user in $users) {           
+#Search in specified OU and Update existing attributes            
+Get-ADUser -Filter "samaccountname -eq '$($user.name)'"  -Properties *  | select description , emailaddress
+}
+
+#| select description , emailaddress
+#set-aduser -EmailAddress $user.mail -Description $user.Description
+#|set-aduser  -replace @{othertelephone= $($user.number)}
+#|select othertelephone
